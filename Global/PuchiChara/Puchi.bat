5abb320bf1b96b5d23175391ae89394704edc6bb
Moshir <alexandermoshir@gmail.com>
2024-03-28 15:11:40 -1000

    Updated Puchi Chracters
    Removed old templates and began adding new Puchis
    Using Magik to extract Puchi's easily from main sprite file

---
+++ Global/PuchiChara/Puchi.bat
@@ -0,0 +1,18 @@
+magick -extract 516x256+0+0 PuchiChara.png "Arashi\Chara.png"
+magick -extract 516x256+516+0 PuchiChara.png "Lantern Eel\Chara.png"
+magick -extract 516x256+1032+0 PuchiChara.png "Fox Kabuki Kid\Chara.png"
+magick -extract 516x256+1548+0 PuchiChara.png "Hyottoko Kabuki Kid\Chara.png"
+magick -extract 516x256+2064+0 PuchiChara.png "Okame Kabuki Kid\Chara.png"
+
+magick -extract 516x256+0+256 PuchiChara.png "Fuuga\Chara.png"
+magick -extract 516x256+516+256 PuchiChara.png "Fuuga and Mirai\Chara.png"
+magick -extract 516x256+1032+256 PuchiChara.png "Princess Soprano\Chara.png"
+magick -extract 516x256+1548+256 PuchiChara.png "Mirai\Chara.png"
+magick -extract 516x256+2064+256 PuchiChara.png "Maou\Chara.png"
+
+magick -extract 516x256+0+512 PuchiChara.png "Princess Soprano and Ralco\Chara.png"
+magick -extract 516x256+516+512 PuchiChara.png "Neko and Shaxy\Chara.png"
+magick -extract 516x256+1032+512 PuchiChara.png "Tatsu-Don\Chara.png"
+magick -extract 516x256+1548+512 PuchiChara.png "Ralco\Chara.png"
+
+
